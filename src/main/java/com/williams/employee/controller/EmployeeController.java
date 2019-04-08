e34e92a8967f09fdb6d5400ea5ea724078469131
Williams <brodwilliams@teksystems.com>
2019-04-08 15:51:43 -0500

    Added Swagger

--- src/main/java/com/williams/employee/controller/EmployeeController.java
+++ src/main/java/com/williams/employee/controller/EmployeeController.java
@@ -10,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.williams.employee.model.Employee;
 import com.williams.employee.service.EmployeeService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/employee")
+@Api(value = "employee")
+public class EmployeeController {
+	@Autowired
+	EmployeeService employeeService;
 
-public class EmployeeController
-{
-    @Autowired
-    EmployeeService employeeService;
- 
+	@ApiOperation(value = "This will return all employees", response = Iterable.class)
 	@GetMapping("/getAllEmployees")
 	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
